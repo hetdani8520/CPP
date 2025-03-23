074af0fc1afbaf306c72c631070387c28622ee66
hetdani8520 <hetdani2021@gmail.com>
2025-03-22 23:31:40 -0700

    c++ compiler installed (mingw) gitignore added to ignore all c++ executables from tracking by git

--- bit_manipulation/extract_bits.cpp
+++ bit_manipulation/extract_bits.cpp
@@ -1,10 +1,10 @@
 //Write a function to extract ‘k’ bits from a given position ‘p’ in a number(num)?
 #include <iostream>
-#using namespace std;
+using namespace std;
 
 int extractbits(int num, int k, int p) {
   // extract k bits from p position of number num
-  // num = 15 --> 1111
+  // num = 14 --> 1110
   // k=2
   // p=1 (index into num & position 1 means index 0)
   return ((num >> (p - 1)) & ((1 << k) - 1));
@@ -15,6 +15,6 @@ int main() {
   int k = 2;
   int p = 1;
   extractbits(num, k, p);
-  cout << "Extracted bits of " << num << "are " << extractbits(num, k, p);
+  cout << "Extracted bits of " << num << " are " << extractbits(num, k, p);
   return 0;
 }
